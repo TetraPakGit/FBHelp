3f067e339464c408a5d09eeed5c6de43ff02c0ff
Petter Mattsson <petter.mattsson@tetrapak.com>
2013-04-05 13:30:09 +0200

    Message update
    Updated message and layout

--- fbdlg.js
+++ fbdlg.js
@@ -1,12 +1,13 @@
 //Opens up a Facebook dialog after a few minutes of surfing.
 
 var triggerTime;
-var $info = $("<div id='fbdlg'><div id='closeBtn'></div><p>Not finding what you are looking for? <br/>Ask us on Facebook and we will try to help.<br/><a href='http://www.facebook.com'><img src='http://www.tetrapak.com/SiteCollectionImages/social%20buttons/facebook_icon.png' alt='Facebook'/></a></p></div>");
+var $info = $("<div id='fbdlg'><div id='closeBtn'></div><p>Not finding what you are looking for? <br/><br/>We are lisitening.<br/><hr/><a href='http://www.facebook.com'><img src='http://www.tetrapak.com/SiteCollectionImages/social%20buttons/facebook_icon.png' alt='Facebook'/></a></p></div>");
 
 var cssObj = {
+		  'font-family' : 'verdana',
 		  'background-color' : '#D8DFEA',
 		  'color' : 'black',
-		  'width' : '180px',		  
+		  'width' : '165px',		  
 		  'border' : '1px solid black',
 		  'z-index' : '100000',
 		  'position' : 'absolute',
