977c053403726ca493dcf270efa400bfcafaedf8
Petter Mattsson <petter.mattsson@tetrapak.com>
2013-04-05 13:04:51 +0200

    New image
    Added image icon and aligned block to the right.

--- fbdlg.js
+++ fbdlg.js
@@ -1,17 +1,17 @@
 //Opens up a Facebook dialog after a few minutes of surfing.
 
 var triggerTime;
-var $info = $("<div id='fbdlg'><div id='closeBtn'></div><p>Not finding what you are looking for? Ask us on Facebook and we will try to help.<a href='http://www.facebook.com'>Facebook icon</a></p></div>");
+var $info = $("<div id='fbdlg'><div id='closeBtn'></div><p>Not finding what you are looking for? <br/>Ask us on Facebook and we will try to help.<br/><a href='http://www.facebook.com'><img src='http://www.tetrapak.com/SiteCollectionImages/social%20buttons/facebook_icon.png' alt='Facebook'/></a></p></div>");
 
 var cssObj = {
 		  'background-color' : '#D8DFEA',
 		  'color' : 'black',
-		  'width' : '280px',		  
+		  'width' : '180px',		  
 		  'border' : '1px solid black',
 		  'z-index' : '100000',
 		  'position' : 'absolute',
+		  'right' : '10px',
 		  'padding':'10px',
-		  'float' : 'left',
 		  'opacity' : '0.5',
   		  'top':'-120px'
 		  
